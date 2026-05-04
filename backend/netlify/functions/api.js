3f37355a3ca60aa839ee7f31a8c3255fa302f40a
Geetansh Agrawal <agrawalgeetansh810@gmail.com>
2026-05-05 04:47:15 +0530

    netlify changes

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -1,4 +1,6 @@
 import serverless from 'serverless-http';
 import app from '../../index.js';
 
-export const handler = serverless(app);
+// If app is imported from an ESM module, it might be nested under .default
+const handlerApp = app.default || app;
+export const handler = serverless(handlerApp);
