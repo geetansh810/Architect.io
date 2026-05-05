5bce36d991d756dfe8261047a25ce327e2f2c39b
Geetansh Agrawal <agrawalgeetansh810@gmail.com>
2026-05-05 13:24:35 +0530

    Fix zip courruption issue

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -3,4 +3,6 @@ import app from '../../index.js';
 
 // If app is imported from an ESM module, it might be nested under .default
 const handlerApp = app.default || app;
-export const handler = serverless(handlerApp);
+export const handler = serverless(handlerApp, {
+  binary: ['application/zip', 'image/*', 'application/octet-stream']
+});
